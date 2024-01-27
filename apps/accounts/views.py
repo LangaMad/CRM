332f52ee8f6bda2fd0a4a75b66107c757f5013f7
LangaMAd <artykovh@gmail.com>
2024-01-27 16:20:27 +0600

    start views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.generics import *
+from .serializers import ManagerSerializer
+from rest_framework.viewsets import *
 
+from .models import *
 # Create your views here.
+
+class ManagerViewSet(ModelViewSet):
+    queryset = Manager.objects.all().order_by('-created_at')
+    serializer_class = ManagerSerializer
+
+
+
+
+
