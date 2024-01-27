8b073d6ffbe88f087dfc9b10aa2de2035f07d680
LangaMAd <artykovh@gmail.com>
2024-01-27 18:06:00 +0600

    create client models

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Client(models.Model):
+    full_name = models.CharField(max_length=255, verbose_name='ФИО клиента')
+    client_number = models.CharField(max_length=15, unique=True, verbose_name='Номер клиента')
+    contract_number = models.CharField(max_length=20, unique=True, verbose_name='Номер договора')
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
